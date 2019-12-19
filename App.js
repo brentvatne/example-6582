08470baf7c10135f93f895bcd0f20bacdf1967cd
Brent Vatne <brentvatne@gmail.com>
2019-12-19 09:59:32 -0800

    example

---
+++ App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Toast from 'react-native-root-toast';
+
+// Add a Toast on screen.
+let toast = Toast.show('This is a message', {
+    duration: Toast.durations.LONG,
+    position: Toast.positions.BOTTOM,
+    shadow: true,
+    animation: true,
+    hideOnPress: true,
+    delay: 0,
+    onShow: () => {
+        // calls on toast\`s appear animation start
+    },
+    onShown: () => {
+        // calls on toast\`s appear animation end.
+    },
+    onHide: () => {
+        // calls on toast\`s hide animation start.
+    },
+    onHidden: () => {
+        // calls on toast\`s hide animation end.
+    }
+});
+
+// You can manually hide the Toast, or it will automatically disappear after a `duration` ms timeout.
+setTimeout(function () {
+    Toast.hide(toast);
+}, 5000);
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
